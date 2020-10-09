fa3ca98b1959abbff76048e69d772903aa17cb4d
NuclearCosmo <mac.macivor@uoit.net>
2020-10-09 02:46:21 -0400

    Base Maze complete
    Just need to add moving floor and walls, add the checkpoints, connect dll, make start and end screen UI then done

--- Assets/code/PluginTester.cs
+++ Assets/code/PluginTester.cs
@@ -9,17 +9,39 @@ public class PluginTester : MonoBehaviour
 
     string filePath = "";
 
+    [StructLayout(LayoutKind.Sequential)]
+    struct playerScoreUnity
+    {
+        public float timeCheckPoint1;
+        public float timeCheckPoint2;
+        public float timeCheckPoint3;
+        public float timeCheckPoint4;
+        public float timeCheckPoint5;
+        public float totalTime;
+        public float timesDied;
+    }
+
+    playerScoreUnity test;
+
+    [DllImport(DLL_NAME)]
+
+    private static extern playerScoreUnity getInfo(int number);
+
     [DllImport(DLL_NAME)]
     private static extern void startTimer();
 
     [DllImport(DLL_NAME)]
     private static extern void setFilePath(string filepath);
 
+    [DllImport(DLL_NAME)]
+    private static extern void setScore(playerScoreUnity info);
+
 
     // Start is called before the first frame update
     void Start()
     {
         filePath = Application.dataPath + "/SaveData/records.txt";
+        test.timeCheckPoint1 = 1;
     }
 
     // Update is called once per frame
@@ -29,6 +51,8 @@ public class PluginTester : MonoBehaviour
         {
             startTimer();
             setFilePath(filePath);
+            setScore(test);
+            Debug.Log(getInfo(0).timeCheckPoint1);
             Debug.Log("NoCrash?");
         }
     }
