965c6645e3af4384cace0817b8b030046a97bff9
NuclearCosmo <mac.macivor@uoit.net>
2020-10-09 04:21:41 -0400

    Maze done, re spawn points, moving walls and platforms
    just need the ui for start and end screens.
    plus the time from file

--- Assets/code/movingPlatformBehavior.cs
+++ Assets/code/movingPlatformBehavior.cs
@@ -7,18 +7,25 @@ public class movingPlatformBehavior : MonoBehaviour
     [Range(0, 100)]
     public float distanceTillReverse = 1.0f;
 
+    [Range(0, 100)]
+    public float speed = 1;
+
     private float distanceMoved = 0.0f;
 
     private float isReversed = 1.0f;
     // Update is called once per frame
     void Update()
     {
-        float distToMove = Vector3.forward.z * Time.deltaTime * isReversed;
+        float distToMove = Vector3.forward.z * speed * Time.deltaTime * isReversed;
         transform.position += new Vector3(0, 0, distToMove);
         distanceMoved += distToMove;
 
-        if (distanceMoved >= distanceTillReverse || distanceMoved <= 0) {
-            isReversed *= -1;
+        if (distanceMoved >= distanceTillReverse) {
+            isReversed = -1;
+        }
+        else if (distanceMoved <= 0)
+        {
+            isReversed = 1;
         }
     }
 }
--- Assets/code/playerBehavior.cs
+++ Assets/code/playerBehavior.cs
@@ -18,19 +18,19 @@ public class playerBehavior : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.W))
         {
-            transform.position += Vector3.forward * Time.deltaTime * 5.0f;
+            transform.position += Vector3.forward * Time.deltaTime * 7.0f;
         }
         else if (Input.GetKey(KeyCode.S))
         {
-            transform.position += Vector3.back * Time.deltaTime * 5.0f;
+            transform.position += Vector3.back * Time.deltaTime * 7.0f;
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.position += Vector3.left * Time.deltaTime * 5.0f;
+            transform.position += Vector3.left * Time.deltaTime * 7.0f;
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.position += Vector3.right * Time.deltaTime * 5.0f;
+            transform.position += Vector3.right * Time.deltaTime * 7.0f;
         }
 
         Physics.gravity = new Vector3(0, Physics.gravity.y, 0);
