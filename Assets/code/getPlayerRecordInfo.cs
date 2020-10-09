0be0a874dc7e0929b51465c90017284be6171c4d
NuclearCosmo <mac.macivor@uoit.net>
2020-10-09 15:42:36 -0400

    Scores done
    Just need to fix the max in the dll from 5 to 6
    Add the score thing

---
+++ Assets/code/getPlayerRecordInfo.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class getPlayerRecordInfo : MonoBehaviour
+{
+    public Text theText;
+    public Text theText2;
+    public Text theText3;
+
+    
+    public int listnum;
+    public int listnum2;
+    public int listnum3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<PluginTester>().setTheFilePath();
+        gameObject.GetComponent<PluginTester>().loadTheValues();
+        theText.text = "'Player'\nCheckPoint1: " + gameObject.GetComponent<PluginTester>().callTheInfo(listnum)[0] + "\nCheckPoint2: " + gameObject.GetComponent<PluginTester>().callTheInfo(listnum)[1] +
+            "\nCheckPoint3: " + gameObject.GetComponent<PluginTester>().callTheInfo(listnum)[2] + "\nCheckPoint4: " + gameObject.GetComponent<PluginTester>().callTheInfo(listnum)[3] +
+            "\nCheckPoint5: " + gameObject.GetComponent<PluginTester>().callTheInfo(listnum)[4] + "\nTotalTime: " + gameObject.GetComponent<PluginTester>().callTheInfo(listnum)[5];
+        theText2.text = "'Player'\nCheckPoint1: " + gameObject.GetComponent<PluginTester>().callTheInfo(listnum2)[0] + "\nCheckPoint2: " + gameObject.GetComponent<PluginTester>().callTheInfo(listnum2)[1] +
+            "\nCheckPoint3: " + gameObject.GetComponent<PluginTester>().callTheInfo(listnum2)[2] + "\nCheckPoint4: " + gameObject.GetComponent<PluginTester>().callTheInfo(listnum2)[3] +
+            "\nCheckPoint5: " + gameObject.GetComponent<PluginTester>().callTheInfo(listnum2)[4] + "\nTotalTime: " + gameObject.GetComponent<PluginTester>().callTheInfo(listnum2)[5];
+        theText3.text = "'Player'\nCheckPoint1: " + gameObject.GetComponent<PluginTester>().callTheInfo(listnum3)[0] + "\nCheckPoint2: " + gameObject.GetComponent<PluginTester>().callTheInfo(listnum3)[1] +
+            "\nCheckPoint3: " + gameObject.GetComponent<PluginTester>().callTheInfo(listnum3)[2] + "\nCheckPoint4: " + gameObject.GetComponent<PluginTester>().callTheInfo(listnum3)[3] +
+            "\nCheckPoint5: " + gameObject.GetComponent<PluginTester>().callTheInfo(listnum3)[4] + "\nTotalTime: " + gameObject.GetComponent<PluginTester>().callTheInfo(listnum3)[5];
+        //gameObject.GetComponent<PluginTester>().callTheInfo(listnum)[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void updateTheInfo()
+    {
+    }
+
+}
--- Assets/code/playerBehavior.cs
+++ Assets/code/playerBehavior.cs
@@ -7,15 +7,18 @@ public class playerBehavior : MonoBehaviour
     private int currentCheckPoint = 0;
     private Vector3 currentRespawnLocation = new Vector3(0, 10, 0);
 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameObject.GetComponent<PluginTester>().setTheFilePath();
+        gameObject.GetComponent<PluginTester>().loadTheValues();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Debug.Log(gameObject.GetComponent<PluginTester>().callTheInfo(0)[0]);
         if (Input.GetKey(KeyCode.W))
         {
             transform.position += Vector3.forward * Time.deltaTime * 7.0f;
@@ -46,6 +49,17 @@ public class playerBehavior : MonoBehaviour
     {
         currentRespawnLocation = newRespawnPoint;
         currentRespawnLocation.y += 10;
+        gameObject.GetComponent<PluginTester>().saveTheCheckpoint(currentCheckPoint);
         currentCheckPoint++;
+    }
+
+    public void startTimer()
+    {
+        gameObject.GetComponent<PluginTester>().startTheTimer();
+    }
+
+    public void updateEnd()
+    {
+        gameObject.GetComponent<getPlayerRecordInfo>().updateTheInfo();
     }
 }
